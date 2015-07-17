627d3e6cb049267ca2f90eb9457c006fadfe68ee
greg gs2 <gregorio.jp.mitchell@gmail.com>
2015-07-17 09:28:31 -0700

    needed to add a gain to hear anything.

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 window.onload = function()
 {
-    // document.write("Hello from js.");
-    playTone(2000);
+    document.write("Hello from js.");
+    playTone(5000);
 }
 
 
 function playTone (freq)
 {
-    var context = new webkitAudioContext();
+    var context = new (window.AudioContext || window.webkitAudioContext)();
     var oscillator = context.createOscillator();
+    var gain = context.createGain();
+    
+    oscillator.connect(gain);
+    gain.connect(context.destination);
+    gain.gain.value = 0.02;
 
-    oscillator.type = 0;
-    oscillator.frequency.value = freq;
-    oscillator.connect(context.destination);
-    oscillator.noteOn && oscillator.noteOn(0);
-
+    oscillator.type = 'square';
+    oscillator.frequency.value = freq;   
+    oscillator.start(0);
+    console.log('did what ');
 }
