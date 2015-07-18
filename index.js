44dbdf3db435ddd2cc04e25c5942b76940b9e80d
greg gs2 <gregorio.jp.mitchell@gmail.com>
2015-07-17 22:16:08 -0700

    can't get slider to link up with oscillator

--- index.js
+++ index.js
@@ -1,22 +1,75 @@
 window.onload = function()
 {
-    document.write("Hello from js.");
-    playTone(5000);
-}
 
-
-function playTone (freq)
-{
     var context = new (window.AudioContext || window.webkitAudioContext)();
     var oscillator = context.createOscillator();
     var gain = context.createGain();
+    var oscRun = 0;
     
+
     oscillator.connect(gain);
     gain.connect(context.destination);
-    gain.gain.value = 0.02;
+    gain.gain.value = 0.05;
+    oscillator.frequency.value = 0;
 
-    oscillator.type = 'square';
+    $("#frequency").css("height",400);
+
+    // setup sliders
+    $("#frequency").slider
+    (
+	{
+	    orientation: 'vertical',
+	    range: "min",
+	    min: 0,
+	    max: 6000,
+	    // height: 4000,
+	    value: 60, // what's this?
+	    slide: function (event, ui )
+	    {
+                // oscillator.stop();
+	        $( "#freq_amount" ).val( ui.value );	
+                console.log(ui.value);
+                // playTone(oscillator,'sine',ui.value,oscRun);
+                
+	    }
+	}
+    );
+
+    $("#volume").slider
+    (
+	{
+	    orientation: 'vertical',
+	    range: "min",
+	    min: 0,
+	    max: 5000,
+	    value: 60, // what's this?
+	    slide: function (event, ui )
+	    {
+		$( "#vol_amount" ).val( ui.value );	
+	    }
+	}
+    );
+
+
+    // playTone(440);
+
+}
+
+
+function playTone (oscillator, type, freq,oscRun)
+{
+    //oscillator.stop();
+    oscillator.type = 'sine';
     oscillator.frequency.value = freq;   
-    oscillator.start(0);
-    console.log('did what ');
+    if ( oscRun )
+    {
+	oscillator.stop();
+	oscillator.start();
+	// oscRun = 1;
+    }
+    else
+    {
+	// oscillator.stop();
+	oscillator.start();
+    }
 }
