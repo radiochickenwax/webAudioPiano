66b6eab9af632b203ee8a81a01d2fcbdfb707ade
greg gs2 <gregorio.jp.mitchell@gmail.com>
2015-07-17 22:16:37 -0700

    tones play, but slider doesn't move

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ window.onload = function()
                 // oscillator.stop();
 	        $( "#freq_amount" ).val( ui.value );	
                 console.log(ui.value);
-                // playTone(oscillator,'sine',ui.value,oscRun);
+                playTone(oscillator,'sine',ui.value,oscRun);
                 
 	    }
 	}
