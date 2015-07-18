f136829235d17e3e625dbe301e9c0b1e0f1769dc
greg gs2 <gregorio.jp.mitchell@gmail.com>
2015-07-18 08:34:29 -0700

    added play button in html

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ window.onload = function()
 {
 
     context = new (window.AudioContext || window.webkitAudioContext)();
-    oscillator = context.createOscillator();
-    var gain = context.createGain();
-
+    oscillator = context.createOscillator();	
+    gain = context.createGain();
     oscillator.connect(gain);
     gain.connect(context.destination);
     gain.gain.value = 0.05;
@@ -12,7 +11,7 @@ window.onload = function()
     oscillator.start();
 
 
-    $("#frequency").css("height",400);
+    // $("#frequency").css("height",400);
 
     // setup sliders
     $("#frequency").slider
