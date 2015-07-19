648abb4833e8dcf122fdf5577ab9e300a17b960b
greg mobile n900t <gregorio.jp.mitchell@gmail.com>
2015-07-19 00:29:59 -0700

    switched .click to mouseup/mousedown

--- index.js
+++ index.js
@@ -1,41 +1,50 @@
 window.onload = function()
 {
     // $("#frequency").css("height",400);
- 
 
+    var context;
+    var oscillator;
+    var gain;
     // setup play button
-    $("td").click( function()
-		   {
-		       // get freq from second word in <td>
-		       
-		       // the split actually gives a 14 element array.
+    $("td").mousedown
+    ( 
+	function()
+	{
+	    context = new (window.AudioContext || window.webkitAudioContext)();
+	    oscillator = context.createOscillator();	
+	    gain = context.createGain();
+	    gain.connect(context.destination);
+	
+
+	    // get freq from second word in <td>
+	    
+	    // the split actually gives a 14 element array.
 		       // var freq = $(this).text().split(" ") ;
-		       var freq = $(this).text().split(" ")[4] ;
-		       console.log(freq);
-		       playTone(freq);
-		   }
-		 );
+
+	    var freq = $(this).text().split(" ")[4] ;
+	    console.log(freq);
+	    playTone(freq,oscillator,gain,context);
+	}
+    )
+	.mouseup
+    (
+	function()
+	{
+	    // console.log(context);
+	    oscillator.stop(0);
+	    context.close();
+	}
+    )
 }
 
-function playTone (freq)
+function playTone (freq,oscillator,gain,context)
 {
-    var context = new (window.AudioContext || window.webkitAudioContext)();
-    var oscillator = context.createOscillator();	
-    var gain = context.createGain();
     oscillator.connect(gain);
-    gain.connect(context.destination);
+
     gain.gain.value = 0.08;
 
     oscillator.start();
 
     oscillator.type = 'sine';
     oscillator.frequency.value = freq;   
-    oscillator.stop(.25);
-    // wait a second, then close context
-    setTimeout( function()
-		{
-		    context.close();
-		},
-		1000
-	      );
 }
