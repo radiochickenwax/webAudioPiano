df9a756901c7482137d46ff176e71c27eb4069e2
greg mobile n900t <gregorio.jp.mitchell@gmail.com>
2015-07-19 00:16:44 -0700

    workaround for the audiocontext overflow problem.

--- index.js
+++ index.js
@@ -19,16 +19,23 @@ window.onload = function()
 
 function playTone (freq)
 {
-    context = new (window.AudioContext || window.webkitAudioContext)();
-    oscillator = context.createOscillator();	
-    gain = context.createGain();
+    var context = new (window.AudioContext || window.webkitAudioContext)();
+    var oscillator = context.createOscillator();	
+    var gain = context.createGain();
     oscillator.connect(gain);
     gain.connect(context.destination);
-    gain.gain.value = 0.05;
+    gain.gain.value = 0.08;
 
     oscillator.start();
 
     oscillator.type = 'sine';
     oscillator.frequency.value = freq;   
-    oscillator.stop(1);
+    oscillator.stop(.25);
+    // wait a second, then close context
+    setTimeout( function()
+		{
+		    context.close();
+		},
+		1000
+	      );
 }
