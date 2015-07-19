7340967cf247baa4270f8389d1e2515d6bf0d59b
greg mobile n900t <gregorio.jp.mitchell@gmail.com>
2015-07-18 23:45:26 -0700

    almost working piano. can't play more than six notes though.  AudioContext isn't being garbage collected.   error: "Uncaught NotSupportedError: Failed to construct 'AudioContext': The number of hardware contexts provided (6) is greater than or equal to the maximum bound (6).".

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
 window.onload = function()
 {
     // $("#frequency").css("height",400);
-    context = new (window.AudioContext || window.webkitAudioContext)();
-    oscillator = context.createOscillator();	
-    gain = context.createGain();
-    oscillator.connect(gain);
-    gain.connect(context.destination);
-    gain.gain.value = 0.05;
-    oscillator.frequency.value = 0;
-    oscillator.start();
+ 
 
     // setup play button
     $("td").click( function()
@@ -19,12 +12,23 @@ window.onload = function()
 		       // var freq = $(this).text().split(" ") ;
 		       var freq = $(this).text().split(" ")[4] ;
 		       console.log(freq);
+		       playTone(freq);
 		   }
 		 );
 }
 
-function playTone (oscillator, type, freq)
+function playTone (freq)
 {
-	oscillator.type = 'sine';
-	oscillator.frequency.value = freq;   
+    context = new (window.AudioContext || window.webkitAudioContext)();
+    oscillator = context.createOscillator();	
+    gain = context.createGain();
+    oscillator.connect(gain);
+    gain.connect(context.destination);
+    gain.gain.value = 0.05;
+
+    oscillator.start();
+
+    oscillator.type = 'sine';
+    oscillator.frequency.value = freq;   
+    oscillator.stop(1);
 }
