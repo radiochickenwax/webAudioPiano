be69e8c7e3e424dd676a2110e41c066ca8e95537
greg mobile n900t <gregorio.jp.mitchell@gmail.com>
2015-07-18 23:25:32 -0700

    setting up js

--- index.js
+++ index.js
@@ -11,79 +11,9 @@ window.onload = function()
     oscillator.start();
 
     // setup play button
-    $("#play").button
-    (
-	{
-	    text:false,   icons:  { primary: 'ui-icon-play'  }
-	}
-    ).click( function()
+    $("td").click( function()
 	     {
-		 // oscillator.stop;
-		 // delete oscillator;
-
-		 oscillator = context.createOscillator();	
-
-		 oscillator.connect(gain);
-		 gain.connect(context.destination);
-		 gain.gain.value = 0.05;
-
-		 oscillator.start();
-	     }
 	   );
-
-    $("#stop").button
-    (
-	{ 
-	    text:false,  icons: { primary: 'ui-icon-stop' }
-	}
-    ).click( function()
-	     {
-		 oscillator.stop();
-		 delete oscillator;
-	     }
-	   );
-
-
-    // setup sliders
-    $("#frequency").slider
-    (
-	{
-	    orientation: 'vertical',
-	    range: "min",
-	    min: 0,
-	    max: 6000,
-	    // height: 4000,
-	    value: 60, // what's this?
-	    slide: function (event, ui )
-	    {
-                // oscillator.stop();
-	        $( "#freq_amount" ).val( ui.value );	                
-                console.log(ui.value);
-                playTone(oscillator,'sine',ui.value);
-
-	    }
-	}
-    );
-
-    $("#volume").slider
-    (
-	{
-	    orientation: 'vertical',
-	    range: "min",
-	    min: 0,
-	    max: 5000,
-	    value: 60, // what's this?
-	    slide: function (event, ui )
-	    {
-		$( "#vol_amount" ).val( ui.value );	
-	    }
-	}
-    );
-
-
-}
-
-
 
 
 function playTone (oscillator, type, freq)
